2f0d369b1f6b1d3850adce4e9f79ff37f53f8ffa
prerana1821 <prerananawar1@gmail.com>
2020-11-28 18:27:28 +0530

    update: ex03 completed

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-let input = prompt('Enter User Input: ');
-alert('Hello ' + input);
+//ex02: using javascript
+// let input = prompt('Enter User Input: ');
+// alert('Hello ' + input);
+
+//ex03: wiring button click
+let inputButton = document.querySelector('#btn-translate');
+console.log(inputButton);
+
+inputButton.addEventListener("click", function userClick() {
+    console.log("click");
+});
