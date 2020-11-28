dab273f4333f305e5d041da88a3013d6845b4c97
prerana1821 <prerananawar1@gmail.com>
2020-11-28 20:16:21 +0530

    update: ex09 completed

--- app.js
+++ app.js
@@ -6,35 +6,50 @@ let inputButton = document.querySelector('#btn-translate');
 let textInput = document.querySelector('textarea');
 let outputText = document.querySelector('#output-text');
 
-// console.log(inputButton);
+let serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-inputButton.addEventListener("click", function userClick() {
-    console.log("click");
-    console.log(textInput.value);
-    outputText.innerText = 'Hello ' + textInput.value;
-});
-
-//live exercise:to fetch data from mock - API
-text1 = "Hello I'm Iron Man";
-text2 = "Hello I'm Shaktimaan Man";
-text3 = "Hello I'm BatMan";
+// console.log(text);
 
 function urlText(text) {
-    return "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" + "?text=" + text;
-}
-
-function urlFetch(text) {
-    fetch(urlText(text)).then(response => response.json()).then(json => console.log(json));
+    // let text = textInput.value;
+    return serverUrl + "?text=" + text;
 }
 
 // function urlFetch(text) {
-//     fetch(urlText(text)).then(function getResponse(response) {
-//         return response.json();
-//     }).then(function getJsonLog(json) {
-//         console.log(json);
-//     });
+//     fetch(urlText(text)).then(response => response.json()).then(json => console.log(json));
 // }
 
-urlFetch(text1);
-urlFetch(text2);
-urlFetch(text3);
+inputButton.addEventListener("click", function userClick() {
+    // console.log("click");
+    // console.log(textInput.value);
+    // outputText.innerText = 'Hello ' + textInput.value;
+    fetch(urlText(textInput.value)).then(response => response.json()).then(function getJsonLog(json) {
+        console.log(json);
+        outputText.innerText = json.contents.text;
+    });;
+});
+
+//live exercise:to fetch data from mock - API
+// text1 = "Hello I'm Iron Man";
+// text2 = "Hello I'm Shaktimaan Man";
+// text3 = "Hello I'm BatMan";
+
+// function urlText(text) {
+//     return "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" + "?text=" + text;
+// }
+
+// function urlFetch(text) {
+//     fetch(urlText(text)).then(response => response.json()).then(json => console.log(json));
+// }
+
+// // function urlFetch(text) {
+// //     fetch(urlText(text)).then(function getResponse(response) {
+// //         return response.json();
+// //     }).then(function getJsonLog(json) {
+// //         console.log(json);
+// //     });
+// // }
+
+// urlFetch(text1);
+// urlFetch(text2);
+// urlFetch(text3);
