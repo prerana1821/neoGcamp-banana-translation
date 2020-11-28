153c6d7c2fa6d9932775b3697eeec2bff95050a4
prerana1821 <prerananawar1@gmail.com>
2020-11-28 19:58:30 +0530

    update: live exercise:to fetch data from mock - API completed

--- app.js
+++ app.js
@@ -4,16 +4,37 @@
 
 let inputButton = document.querySelector('#btn-translate');
 let textInput = document.querySelector('textarea');
+let outputText = document.querySelector('#output-text');
 
 // console.log(inputButton);
 
 inputButton.addEventListener("click", function userClick() {
     console.log("click");
     console.log(textInput.value);
+    outputText.innerText = 'Hello ' + textInput.value;
 });
 
-//live exercise: querySelector
-// 1. document.querySelector('textarea');
-// 2. document.querySelector('.btn-primary');
-// 3. document.querySelector('#input-btn');
-// 4. document.querySelector('input[name="translator"]');
+//live exercise:to fetch data from mock - API
+text1 = "Hello I'm Iron Man";
+text2 = "Hello I'm Shaktimaan Man";
+text3 = "Hello I'm BatMan";
+
+function urlText(text) {
+    return "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" + "?text=" + text;
+}
+
+function urlFetch(text) {
+    fetch(urlText(text)).then(response => response.json()).then(json => console.log(json));
+}
+
+// function urlFetch(text) {
+//     fetch(urlText(text)).then(function getResponse(response) {
+//         return response.json();
+//     }).then(function getJsonLog(json) {
+//         console.log(json);
+//     });
+// }
+
+urlFetch(text1);
+urlFetch(text2);
+urlFetch(text3);
